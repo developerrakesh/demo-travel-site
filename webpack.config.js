5dd01847a5d03709d2edb4a04dfc7b5838aa18bd
Rakesh <rakeshwins232@gmail.com>
2020-11-25 13:16:25 +0530

    initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+let path = require('path');
+
+module.exports = {
+    entry: './app/assets/scripts/app.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader', 
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    'autoprefixer'
+                                ]
+                            }
+                        }
+                    }, 
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+}
