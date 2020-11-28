cac53e74653c5d832fee0d2112f623bd4d168223
Rakesh <rakeshwins232@gmail.com>
2020-11-28 16:25:53 +0530

    applied lazyload to images

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,5 @@
 import '../styles/main.scss';
+import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
