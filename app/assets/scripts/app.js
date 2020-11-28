5ee9892d1f8a34966be3964805a57770d6221621
Rakesh <rakeshwins232@gmail.com>
2020-11-28 15:58:30 +0530

    completed modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,10 +3,25 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
-const mobileMenu = new MobileMenu();
-const revealOnScrollforFeatureItem = new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
-const revealOnScrollforTestimonial = new RevealOnScroll(document.querySelectorAll('.testimonial'), 65);
+new MobileMenu();
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 65);
 const stikcyHeader = new StickyHeader();
+let modal;
+
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', evt => {
+        evt.preventDefault();
+        if(typeof modal == 'undefined') {
+            import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openModal(), 50);
+            }).catch(() => console.log('Problem in Loading Modal'));
+        } else {
+            modal.openModal();
+        }
+    });
+});
 
 if (module.hot) {
     module.hot.accept();
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,53 @@
+class Modal {
+    constructor() {
+        this.injectHTML();
+        this.closeIcon = document.querySelector('.modal__close');
+        this.modal = document.querySelector('.modal');
+        this.events();
+    }
+
+    injectHTML() {
+        document.body.insertAdjacentHTML('beforeend', `
+            <div class="modal">
+                <div class="modal__inner">
+                <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
+                <div class="wrapper wrapper--narrow">
+                    <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
+                </div>
+            
+                <div class="social-icons">
+                    <a href="#" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
+                    <a href="#" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
+                    <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
+                    <a href="#" class="social-icons__icon"><img src="assets/images/icons/youtube.svg" alt="YouTube"></a>
+                </div>
+                </div>
+                <div class="modal__close">X</div>
+            </div>
+        `);
+    }
+
+    openModal() {
+        this.modal.classList.add('modal--show');
+    }
+
+    hideModal() {
+        this.modal.classList.remove('modal--show');
+    }
+
+    keyPressHandler(evt) {
+        if (evt.keyCode == 27) {
+            this.hideModal();
+        }
+    }
+
+    events() {
+        //listen for close click
+        this.closeIcon.addEventListener('click', () => this.hideModal());
+
+        //listen for esc key press
+        document.addEventListener('keyup', evt => this.keyPressHandler(evt));
+    }
+}
+
+export default Modal;
