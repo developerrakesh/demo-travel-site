5a80a3e937fd620e35f35cc6e20862e4f532021c
Rakesh <rakeshwins232@gmail.com>
2020-11-29 17:19:35 +0530

    Fixing CORS issue for local dev

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,17 @@
 exports.handler = function(event, context, callback) {
+    const headers = {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Headers" : "Content-Type"
+    }
+    
+    if (event.httpMethod !== "POST") {
+    return callback(null, {
+        statusCode: 200,
+        headers,
+        body: "This was not a POST request"
+    })
+    }
+
     const secretContent = `
         <h3>Welcom To The Secret Area</h3>
         <p>The <strong>sky</strong> is blue</p>
