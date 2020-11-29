a7ed88ed2996c00555aa1a0257d8e880eda949a0
Rakesh <rakeshwins232@gmail.com>
2020-11-29 17:09:47 +0530

    completed secret areat

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,10 +3,12 @@ import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import ClientArea from './modules/ClientArea';
 
 new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 65);
+new ClientArea();
 const stikcyHeader = new StickyHeader();
 let modal;
 
