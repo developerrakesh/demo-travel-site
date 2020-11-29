795c734ae8a58287a35dc7f145a1c6e26f17571d
Rakesh <rakeshwins232@gmail.com>
2020-11-29 16:20:02 +0530

    added password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,22 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    });
+    const secretContent = ``;
+    
+    let body;
+
+    if(event.body){
+        body = JSON.parse(event.body);
+    } else {
+        body = {};
+    }
+
+    if(body.password == "rakesh") {
+        callback(null, {
+            statusCode: 200,
+            body: "Welcome to the super secret area"
+        });
+    } else {
+        callback(null, {
+            statusCode: 401
+        });
+    }
 }
